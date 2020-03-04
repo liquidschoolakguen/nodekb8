c2243251ebc685c9c8de783370d07775684c897e
liquidschoolakguen <bubu000051@gmail.com>
2020-03-04 11:37:35 +0100

    Final von 04.03

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,9 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 
+//User model
+let User = require('../models/user');
 
-
-
+// Get Single Article
+router.get('/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+      User.findById(article.author, function(err, user){
+          res.render('article', {
+          article:article,
+          author: user.name
+       });
+      }); 
+      
+    });
+});
 
 
 
