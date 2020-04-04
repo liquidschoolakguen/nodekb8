b88e5df1a054986d5c0d97d328b90410b5976e4e
liquidschoolakguen <bubu000051@gmail.com>
2020-04-04 04:26:08 +0200

    hallo

--- routes/articles.js
+++ routes/articles.js
@@ -28,16 +28,6 @@ const handleError = (err, res) => {
 
 
 
-// SET STORAGE
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
-
 
 const upload = multer({
   dest: "../uploads"
